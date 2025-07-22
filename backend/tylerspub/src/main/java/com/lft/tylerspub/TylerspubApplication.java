42c667ac49405fdeb42dc7198c5bd2279e53d568
cousinbigcountry <fountainkylec@gmail.com>
2025-07-21 22:21:10 -0400

    Backend tylers pub java spring boot set up

---
+++ backend/tylerspub/src/main/java/com/lft/tylerspub/TylerspubApplication.java
@@ -0,0 +1,13 @@
+package com.lft.tylerspub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TylerspubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TylerspubApplication.class, args);
+	}
+
+}
---
+++ backend/tylerspub/src/test/java/com/lft/tylerspub/TylerspubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lft.tylerspub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TylerspubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
